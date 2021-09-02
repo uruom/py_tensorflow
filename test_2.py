a85c9e78bb4061667f1fa38653d44dfadbc74731
uruom <71624685+uruom@users.noreply.github.com>
2021-09-02 12:32:12 +0800

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,12 +1,11 @@
-from numpy.core.fromnumeric import product
 import tensorflow as tf
 
 matrix1 =tf.constant([[3,3]])
-matrix2 =tf.constant([[2],
-                            [2]])
+matrix2 =tf.constant([[2],[2]])#最后答案是[[12]],其实就是矩阵乘法，(3,3)* 2  最后答案就是12
+#                                                                         2  
 product = tf.matmul(matrix1,matrix2)  #matrix multiply np.dot(m1,m2)
 
-###method 1
+###method 1 两个不同的输出方式
 #sess= tf.Session()
 #result =sess.run(product)
 #print(result)
